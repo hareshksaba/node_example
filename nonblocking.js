d0bb3301fcfe622b904a33511a16fb044b9e7ca9
Haresh <hareshdevops@gmail.com>
2017-11-10 10:27:54 -0800

    adding node files

---
+++ nonblocking.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+fs.readFile('text.txt', function(err,data){
+if(err){
+    console.log(err);
+}
+setTimeout(()=>{
+    console.log("Display after 2 seconds")
+},2000);
+
+console.log(data.toString())
+});
+
+
+
+console.log('Start Here');
+
